3e4b11ccb1025f46271046c9106c1d7604066f4c
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-20 15:36:54 +0200

    add video to predictions

--- pages/5_Predictions.py
+++ pages/5_Predictions.py
@@ -5,6 +5,11 @@ from utils import set_seed, load_csv
 st.set_page_config(page_title="Titanic - Predictions")
 st.header("Prédictions")
 
+# URL de la vidéo
+video_url = "https://www.youtube.com/watch?v=AzmdpGuIiZ4"
+
+st.video(video_url)
+
 
 set_seed()
 
@@ -26,6 +31,13 @@ st.dataframe(df)
 st.caption(
     f"Les chances de survie des passagers sont évaluées par les prédictions du classifieur {model_choisi} optimisé."
 )
+
+if st.button("Fin du voyage"):
+    if len(st.session_state.pages) == 5:
+        st.session_state.pages.append(
+            st.Page("pages/6_Terminus.py", title="Terminus", icon="🏁")
+        )
+    st.switch_page(st.session_state.pages[5])
 
 st.markdown(
     """
