d0b8b4392684ff614e14a060114d44f7e34b2ae6
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-25 23:38:52 +0200

    update "About" section

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(
-    menu_items={
-        "Get Help": None,
-        "Report a bug": None,
-        "About": """
-## Titanic Survival Predictor  
-Ce projet prédit les chances de survie des passagers du Titanic grâce au **machine learning**.  
-
-🔍 Code source : [GitHub](https://github.com/DidierFlamm/titanic-survival-predictor)  
-✉️ Contact : [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com)  
-🔗 LinkedIn : [didier-flamm](https://www.linkedin.com/in/didier-flamm)  
-📁 Portfolio : [Streamlit Community Cloud](https://share.streamlit.io/user/didierflamm)  
-
-© 2025 Didier Flamm
-""",
-    }
-)
-
 st.logo(
     # "https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp",
     "https://img.icons8.com/?size=100&id=s5NUIabJrb4C&format=png&color=000000",
     size="large",
 )
+
+
 st.sidebar.subheader("Language", divider=True)
 
 language = st.sidebar.selectbox(
@@ -40,10 +24,42 @@ else:
     st.sidebar.subheader("Music", divider=True)
 
 iframe_code = """
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/97158016&color=%231a4b75&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/maymon-abdullah" title="Maymon Abdullah" target="_blank" style="color: #cccccc; text-decoration: none;">Maymon Abdullah</a> · <a href="https://soundcloud.com/maymon-abdullah/titanic-theme-song-flute-instrumental" title="Titanic Theme Song &quot;My Heart Will Go On&quot; - Flute Instrumental - Karin Leitner" target="_blank" style="color: #cccccc; text-decoration: none;">Titanic Theme Song &quot;My Heart Will Go On&quot; - Flute Instrumental - Karin Leitner</a></div>"""
+<iframe width="100%" height="130" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/97158016&color=%231a4b75&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>"""
 
 with st.sidebar:
-    components.html(iframe_code)
+    components.html(iframe_code, height=120)
+
+st.sidebar.divider()
+st.sidebar.markdown(
+    """
+<div style='text-align: center'>
+  <a href="https://share.streamlit.io/user/didierflamm" target="_blank">
+    <img src="https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp" style="max-width: 100%; height: auto;" width="150"/>
+  </a>
+</div>
+""",
+    unsafe_allow_html=True,
+)
+
+st.set_page_config(
+    menu_items={
+        "Get Help": None,
+        "Report a bug": None,
+        "About": (
+            """## Titanic Survival Predictor  
+Ce projet prédit les chances de survie des passagers du Titanic grâce au **machine learning**. Le code source est disponible sur  
+"""
+            if st.session_state.lang == "fr"
+            else """## Titanic Survival Predictor  
+This project predicts the survival chances of Titanic passengers using machine learning. The source code is available on """
+        )
+        + """[GitHub](https://github.com/DidierFlamm/titanic-survival-predictor)  
+
+© 2025 Didier Flamm  
+✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) – 🔗 [LinkedIn](https://www.linkedin.com/in/didier-flamm) – 📁 [Portfolio](https://share.streamlit.io/user/didierflamm)  
+""",
+    }
+)
 
 if "pages" not in st.session_state:
     st.session_state.pages = [
@@ -57,18 +73,6 @@ if "pages" not in st.session_state:
         # st.Page("pages/3_Evaluation.py", title="Evaluation", icon="📝"),
         # st.Page("pages/4_Optimisation.py", title="Optimisation", icon="📈"),
     ]
-
-st.sidebar.divider()
-st.sidebar.markdown(
-    """
-<div style='text-align: center'>
-  <a href="https://share.streamlit.io/user/didierflamm" target="_blank">
-    <img src="https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp" style="max-width: 100%; height: auto;" width="150"/>
-  </a>
-</div>
-""",
-    unsafe_allow_html=True,
-)
 
 pg = st.navigation(st.session_state.pages, position="top")
 pg.run()
