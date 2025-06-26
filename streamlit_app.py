f165fbf8af35731ede1486a4e34cd04bd5347cf2
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-26 12:06:42 +0200

    Update ambiance + add all pages in navigation at once

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,23 +20,64 @@ st.session_state.lang = lang
 
 st.sidebar.subheader("Ambiance", divider=True)
 
-iframe_code = """<iframe width="100%" height="130" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1851039789&color=%231a4b75&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe></div>"""
-# """<iframe width="100%" height="130" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/97158016&color=%231a4b75&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>"""
+ambiance = st.sidebar.radio("Select ambiance", ("🎛️ Trance remix", "💿 Titanic OST"))
 
+if ambiance.startswith("🎛️"):
+    iframe_code = """<iframe width="100%" height="130" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1851039789&color=%231a4b75&auto_play=true&hide_related=True&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe>"""
+    # """<iframe width="100%" height="130" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/97158016&color=%231a4b75&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe>"""
+    with st.sidebar:
+        components.html(iframe_code, height=120)
+else:
+    tracks = {
+        "1. Never an Absolution": "https://archive.org/download/TitanicMusicfromtheMotionPicture/01%20Never%20an%20Absolution.mp3",
+        "2. Distant Memories": "https://archive.org/download/TitanicMusicfromtheMotionPicture/02%20Distant%20Memories.mp3",
+        "3. Southampton": "https://archive.org/download/TitanicMusicfromtheMotionPicture/03%20Southampton.mp3",
+        "4. Rose": "https://archive.org/download/TitanicMusicfromtheMotionPicture/04%20Rose.mp3",
+        "5. Leaving Port": "https://archive.org/download/TitanicMusicfromtheMotionPicture/05%20Leaving%20Port.mp3",
+        '6. "Take Her to Sea, Mr. Murdoch"': "https://archive.org/download/TitanicMusicfromtheMotionPicture/06%20%22Take%20Her%20to%20Sea%2C%20Mr.%20Murdoch%22.mp3",
+        '7. "Hard to Starboard"': "https://archive.org/download/TitanicMusicfromtheMotionPicture/07%20Track07.mp3",
+        "8. Unable to Stay, Unwilling to Leave": "https://archive.org/download/TitanicMusicfromtheMotionPicture/08%20Unable%20to%20Stay%2C%20Unwilling%20to%20Leave.mp3",
+        "9. The Sinking": "https://archive.org/download/TitanicMusicfromtheMotionPicture/09%20The%20Sinking.mp3",
+        "10. Death of Titanic": "https://archive.org/download/TitanicMusicfromtheMotionPicture/10%20Death%20of%20Titanic.mp3",
+        "11. A Promise Kept": "https://archive.org/download/TitanicMusicfromtheMotionPicture/11%20A%20Promise%20Kept.mp3",
+        "12. A Life So Changed": "https://archive.org/download/TitanicMusicfromtheMotionPicture/12%20A%20Life%20So%20Changed.mp3",
+        "13. An Ocean of Memories": "https://archive.org/download/TitanicMusicfromtheMotionPicture/13%20An%20Ocean%20of%20Memories.mp3",
+        "14. My Heart Will Go On": "https://archive.org/download/TitanicMusicfromtheMotionPicture/14%20My%20Heart%20Will%20Go%20On.mp3",
+        "15. Hymn to the Sea": "https://archive.org/download/TitanicMusicfromtheMotionPicture/15%20Hymn%20to%20the%20Sea.mp3",
+    }
 
-with st.sidebar:
-    components.html(iframe_code, height=120)
+    if "track_index" not in st.session_state:
+        st.session_state.track_index = 0
+
+    track = st.sidebar.selectbox(
+        "Select track",
+        list(tracks.keys()),
+        index=st.session_state.track_index,
+    )
+
+    st.session_state.track_index = list(tracks.keys()).index(track)
+
+    st.sidebar.audio(
+        tracks[track],
+        format="audio/mpeg",  # = mp3
+        autoplay=True,
+    )
+
 
 st.sidebar.subheader("View all apps", divider=True)
-st.sidebar.markdown(
-    """
-<div style='text-align: center'>
-  <a href="https://share.streamlit.io/user/didierflamm" target="_blank">
-    <img src="https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp" style="max-width: 100%; height: auto;" width="150"/>
-  </a>
-</div>
-""",
-    unsafe_allow_html=True,
+# st.sidebar.markdown(
+#    """
+# <div style='text-align: center'>
+#  <a href="https://share.streamlit.io/user/didierflamm" target="_blank">
+#    <img src="https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp" style="max-width: 100%; height: auto;" width="150"/>
+#  </a>
+# </div>
+# """,
+#    unsafe_allow_html=True,
+# )
+
+st.sidebar.image(
+    "https://raw.githubusercontent.com/DidierFlamm/DidierFlamm/main/dids.webp"
 )
 
 st.set_page_config(
@@ -67,9 +108,11 @@ if "pages" not in st.session_state:
             icon="⚓",
             default=True,
         ),
-        # st.Page("pages/2_Visualisation.py", title="Visualisation", icon="📊"),
-        # st.Page("pages/3_Evaluation.py", title="Evaluation", icon="📝"),
-        # st.Page("pages/4_Optimisation.py", title="Optimisation", icon="📈"),
+        st.Page("pages/2_Visualisation.py", title="Visualisation", icon="📊"),
+        st.Page("pages/3_Evaluation.py", title="Evaluation", icon="📝"),
+        st.Page("pages/4_Optimisation.py", title="Optimisation", icon="📈"),
+        st.Page("pages/5_Predictions.py", title="Predictions", icon="🎯"),
+        st.Page("pages/6_Terminus.py", title="Terminus", icon="🏁"),
     ]
 
 pg = st.navigation(st.session_state.pages, position="top")
