24439aa8eeda915dbaf7df0f98a784b28b2d44df
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-29 15:34:23 +0200

    change to plotly

--- pages/2_Visualisation.py
+++ pages/2_Visualisation.py
@@ -14,17 +14,116 @@ df = load_csv(drop_outliers=False)
 
 df_display = df.copy()
 
-df_display["Sex"] = df_display["Sex"].replace({"female": "Femme", "male": "Homme"})
-df_display["Survived"] = df_display["Survived"].replace({0: "Non", 1: "Oui"})
+df_display["Sex"] = df_display["Sex"].map({"female": "Femme", "male": "Homme"})
+df_display["Survived"] = df_display["Survived"].map({0: "Non", 1: "Oui"})
 
 palette = sns.color_palette("RdYlGn", n_colors=3)  # rouge - jaune - vert
 palette = [palette[2], palette[0]]  # vert et rouge
 
 st.subheader(":blue[Analyse univariée]", divider=True)
+
+(
+    tab_survived,
+    tab_sex,
+    tab_age,
+    tab_class,
+    tab_fare,
+    tab_sibsp,
+    tab_parch,
+    tab_embark,
+) = st.tabs(
+    [
+        "Survie (cible)",
+        "Sexe",
+        "Age",
+        "Classe",
+        "Tarif",
+        "Fratrie & conjoint(e)",
+        "Parents & enfants",
+        "Embarquement",
+    ]
+)
+
+with tab_survived:
+    fig = px.pie(
+        df_display,
+        names="Survived",
+        category_orders={"Survived": ["Oui", "Non"]},
+        title="Répartition des survivants",
+    )
+    st.plotly_chart(fig)
+
+with tab_sex:
+    fig = px.pie(
+        df_display,
+        names="Sex",
+        category_orders={"Sex": ["Femme", "Homme"]},
+        title="Répartition des genres",
+    )
+    st.plotly_chart(fig)
+
+with tab_age:
+    fig = px.histogram(
+        df_display,
+        x="Age",
+        title="Distribution des âges",
+    )
+    st.plotly_chart(fig)
+
+with tab_class:
+    fig = px.pie(
+        df_display,
+        names="Pclass",
+        category_orders={"Pclass": [1, 2, 3]},
+        title="Répartition des classes",
+    )
+    st.plotly_chart(fig)
+
+with tab_fare:
+    fig = px.histogram(
+        df_display,
+        x="Fare",
+        title="Distribution des tarifs",
+    )
+    st.plotly_chart(fig)
+    st.write(
+        "Trois passagers présentent un tarif de £512.33, nettement supérieur à la distribution générale. Bien que ces valeurs extrêmes ne soient pas nécessairement aberrantes, elles sont considérées comme des outliers et seront exclues du jeu de données afin d'éviter qu’elles ne biaisent les résultats ultérieurs."
+    )
+
+with tab_sibsp:
+    fig = px.pie(
+        df_display,
+        names="SibSp",
+        category_orders={"SibSp": sorted(df_display.SibSp.unique())},
+        title="Répartition du nombre de frères, sœurs et conjoint(e)",
+    )
+    st.plotly_chart(fig)
+
+with tab_parch:
+    fig = px.pie(
+        df_display,
+        names="Parch",
+        category_orders={"Parch": sorted(df_display.Parch.unique())},
+        title="Répartition du nombre de parents et enfants",
+    )
+    st.plotly_chart(fig)
+
+with tab_embark:
+    fig = px.pie(
+        df_display,
+        names="Embarked",
+        # category_orders={"Parch": sorted(df_display.Parch.unique())},
+        title="Répartition des ports d'embarquement",
+    )
+    st.plotly_chart(fig)
+
+
 fig, axs = plt.subplots(1, 3, figsize=(12, 4))
 sns.countplot(
     x="Survived", data=df_display, order=["Oui", "Non"], palette=palette, ax=axs[0]
 )
+
+
 axs[0].set_xlabel("Survie")
 for ax in axs:
     ax.set_ylabel("Nombre de passagers")
