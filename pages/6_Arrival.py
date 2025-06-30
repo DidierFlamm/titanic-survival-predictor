7b464134e1a2502829815490efedfd78c69c8ac6
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-30 04:19:15 +0200

    Chore

--- pages/6_Arrival.py
+++ pages/6_Arrival.py
@@ -14,21 +14,19 @@ video_url = "https://youtu.be/Sj9MEwjkxE0"
 
 st.video(video_url, autoplay=True, muted=True)
 
-st.write("🚧 WIP")
-st.write("merci ...")
+st.subheader("🎉🚢")
+
+st.write(
+    """Au nom de tout l’équipage, je souhaite vous adresser mes plus sincères félicitations et remerciements pour avoir bravé avec succès cet océan de données. J’espère que ce projet vous a apporté autant de plaisir que d’apprentissage, et j’ai hâte de vous retrouver très bientôt pour de nouvelles aventures passionnantes avec la compagnie DIDS !
+"""
+)
 
 st.markdown(
-    'Le code source est disponible sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>',
+    """Vous pouvez consulter le code source de Titanic Survival Predictor sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>, ainsi que découvrir toutes mes applications interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>. N’hésitez pas à y faire un tour et à me faire part de vos retours par mail ✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) ou sur 🔗 [LinkedIn](https://www.linkedin.com/in/didier-flamm)""",
     unsafe_allow_html=True,
 )
 
-
-st.markdown(
-    'Retrouvez toutes mes applis interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>',
-    unsafe_allow_html=True,
-)
-
-st.write("WIP 🚧")
+st.subheader(":blue[A bientôt !]")
 
 st.divider()
 st.markdown(
