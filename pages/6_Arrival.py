9395800b2d5ae510db03715b48eea06ca3467f51
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-07-01 04:26:45 +0200

    add write_stream

--- pages/6_Arrival.py
+++ pages/6_Arrival.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from utils import stream_data
 
 st.markdown(
     "<h2 style='text-align: center; color: #0366d6;'>🏁 Port of arrival</h2>",
@@ -16,13 +16,15 @@ st.video(video_url, autoplay=True, muted=True)
 
 st.subheader("🎉🚢")
 
-st.write(
-    """Au nom de tout l’équipage, je souhaite vous adresser mes plus sincères félicitations et remerciements pour avoir bravé avec succès cet océan de données. J’espère que ce projet vous a apporté autant de plaisir que d’apprentissage, et j’ai hâte de vous retrouver très bientôt pour de nouvelles aventures passionnantes avec la compagnie DIDS !
+st.write_stream(
+    stream_data(
+        """Au nom de tout l’équipage, je souhaite vous adresser mes plus sincères **félicitations** et **remerciements** pour avoir bravé avec succès cet océan de données en notre compagnie. J’espère que ce projet vous a apporté autant de plaisir que d’apprentissage, et j’ai hâte de vous retrouver très bientôt pour de nouvelles aventures passionnantes avec **DIDS** !
 """
+    )
 )
 
 st.markdown(
-    """Vous pouvez consulter le code source de Titanic Survival Predictor sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>, ainsi que découvrir toutes mes applications interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>. N’hésitez pas à y faire un tour et à me faire part de vos retours par ✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) ou sur [LinkedIn](https://www.linkedin.com/in/didier-flamm).""",
+    """_Vous pouvez consulter le code source de Titanic Survival Predictor sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>, ainsi que découvrir toutes mes applications interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>. N’hésitez pas à y faire un tour et à me faire part de vos retours par ✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) ou sur [LinkedIn](https://www.linkedin.com/in/didier-flamm)._""",
     unsafe_allow_html=True,
 )
 
