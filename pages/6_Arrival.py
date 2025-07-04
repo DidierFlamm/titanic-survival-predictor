f599e125706a8bd83c1352ab28a0cd15b10526cf
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-07-04 17:09:39 +0200

    style

--- pages/6_Arrival.py
+++ pages/6_Arrival.py
@@ -31,7 +31,8 @@ st.write("**Didier**")
 st.divider()
 
 st.markdown(
-    """_Le code source du projet Titanic Survival Predictor est disponible sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>, et vous pouvez découvrir toutes mes applications interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>. N’hésitez pas à y faire un tour puis me faire part de vos impressions via ✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) ou [LinkedIn](https://www.linkedin.com/in/didier-flamm)._""",
+    """_Le code source du projet Titanic Survival Predictor est disponible sur <a href="https://github.com/DidierFlamm/titanic-survival-predictor" target="_blank">GitHub</a>.  
+    Découvrez toutes mes applications interactives sur <a href="https://share.streamlit.io/user/didierflamm" target="_blank">Streamlit Community Cloud</a>. N’hésitez pas à y faire un tour puis me faire part de vos impressions via ✉️ [didier.flamm@gmail.com](mailto:didier.flamm@gmail.com) ou [LinkedIn](https://www.linkedin.com/in/didier-flamm)._""",
     unsafe_allow_html=True,
 )
 
